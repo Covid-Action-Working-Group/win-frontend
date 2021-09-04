4cce5bd1d904cf7f35b95b536ba692a67bfc0bc0
Abhishek Upperwal <a.upperwal@gmail.com>
2021-09-04 19:08:30 +0530

    Deploy gh-pages

---
+++ _next/static/v9cFiIVQj_mMbJ1nhPufT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ccfc7bf6c9d89084c580.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],"/qr":["static/chunks/pages/qr-4bf97075fc0196425a48.js"],sortedPages:["/","/_app","/_error","/qr"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
