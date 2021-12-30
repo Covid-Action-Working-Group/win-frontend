938232ad0fe1f1aac9a12f052e9226fc7ae9c142
Abhishek Upperwal <a.upperwal@gmail.com>
2021-12-31 00:11:52 +0530

    new subdomain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    assetPrefix: isProd ? 'https://win.cov.social' : '',
+    assetPrefix: isProd ? 'https://win.niua.org' : '',
     // future: { webpack5: true }
     webpack(config) {
         config.module.rules.push({
